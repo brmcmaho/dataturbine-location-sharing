1fde1109cbf12eb656d63c7555dbb575795713b8
brmcmaho <brmcmaho@gmail.com>
2014-10-04 19:10:31 -0700

    Moved DT stuff to activity

--- app/src/main/java/brmcmaho/dtlocationsharing/ActMain.java
+++ app/src/main/java/brmcmaho/dtlocationsharing/ActMain.java
@@ -1,10 +1,18 @@
 package brmcmaho.dtlocationsharing;
 
+import android.content.Context;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import com.rbnb.sapi.SAPIException;
+
 import org.actimo.activity.core.FeatureActivity;
+
+import edu.ucsd.rbnb.simple.MIME;
+import edu.ucsd.rbnb.simple.SimpleSource;
 
 
 public class ActMain extends FeatureActivity  {
@@ -12,15 +20,14 @@ public class ActMain extends FeatureActivity  {
     private FragMain mViewFrag;
     private FLocation mFLocation;
 
+    private SimpleSource src;
+    private Context mContext;
+
     @Override
     protected void initializeFeatures() {
 
         mViewFrag = new FragMain();
-
-
-
-
-
+        mContext = this;
 
 
         mFLocation = new FLocation(new FLocationListener() {
@@ -39,7 +46,7 @@ public class ActMain extends FeatureActivity  {
 
             @Override
             public void locationUpdate(Location location) {
-
+                new SendLocationTask(mContext).execute(location); //TODO
 
                 mViewFrag.locationUpdate(location);
             }
@@ -54,8 +61,11 @@ public class ActMain extends FeatureActivity  {
 
         addFeature(mFLocation);
 
-
     }
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,11 +78,14 @@ public class ActMain extends FeatureActivity  {
                     .add(R.id.main_container, mViewFrag)
                     .commit();
         }
+
+        new ConnectToServerTask(this).execute();
     }
 
-
+//TODO onClick methods
     public void getLocation(View v) {
         mFLocation.getLocation();
+
     }
 
 
@@ -87,6 +100,79 @@ public class ActMain extends FeatureActivity  {
     public void stopUpdates(View v) {
         mFLocation.stopUpdates();
     }
+
+
+
+
+
+
+    protected class ConnectToServerTask extends AsyncTask<Void, Void, String> {
+
+        Context localContext;
+        public ConnectToServerTask(Context context) {
+            super();
+            localContext = context;
+        }
+
+
+        @Override
+        protected String doInBackground(Void... params) {
+            src = new SimpleSource("LocationTester", "76.176.187.138", 3333);
+            src.setConnectionHandling(false);
+
+            try {
+                src.setArchiveSize(400);
+                src.setCacheSize(10);
+                src.addChannel("gps", MIME.GPS);
+                src.connect();
+                Log.e("test", "connected?");
+            } catch (SAPIException e) {
+                Log.e("SAPIException", "on connect", e);
+            }
+            return "";
+        }
+
+        @Override
+        protected void onPostExecute(String address) {
+
+        }
+    }
+
+
+
+
+
+    protected class SendLocationTask extends AsyncTask<Location, Void, String> {
+
+        Context localContext;
+
+        public SendLocationTask(Context context) {
+            super();
+            localContext = context;
+        }
+
+        @Override
+        protected String doInBackground(Location... params) {
+            double time = System.currentTimeMillis()/1000;
+            try {
+                src.put("gps", new double[]{params[0].getLatitude(), params[0].getLongitude()}, time);
+                src.flush();
+                Log.i("DT", "Putting data: "+params[0].getLatitude()+", " +params[0].getLongitude());
+            } catch (SAPIException e) {
+                Log.e("SAPIException", "on data", e);
+            }
+            return "";
+        }
+
+        @Override
+        protected void onPostExecute(String address) {
+
+        }
+    }
+
+
+
+
 
 
 }
